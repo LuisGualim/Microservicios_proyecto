74d14c2a9f362df14578989e7316c7e163d0e41c
LuisGualim <gualimisem@gmail.com>
2025-10-30 21:47:41 -0600

    Arreglo MS

--- src/main/java/com/lg/taller/repository/ClienteRepository.java
+++ src/main/java/com/lg/taller/repository/ClienteRepository.java
@@ -3,5 +3,4 @@ package com.lg.taller.repository;
 import com.lg.taller.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository <Cliente, Long> {
-}
+public interface ClienteRepository extends JpaRepository<Cliente, Long> { }
--- src/main/java/com/lg/taller/service/ClienteService.java
+++ src/main/java/com/lg/taller/service/ClienteService.java
@@ -1,13 +1,22 @@
 package com.lg.taller.service;
 
 import com.lg.taller.entity.Cliente;
+import com.lg.taller.repository.ClienteRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface ClienteService {
-    Cliente guardar(Cliente cliente);
-    List<Cliente> listar();
-    Optional<Cliente> obtenerPorId(Long id);
-    Cliente actualizar(Long id, Cliente cliente);
-    void eliminar(Long id);
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente guardar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
 }
+
